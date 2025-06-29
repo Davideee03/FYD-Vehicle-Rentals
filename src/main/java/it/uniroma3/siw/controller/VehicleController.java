0829bc96077b6d07658d98a8aac0d4da47e9e948
FConsalvi <188714287+FConsalvi@users.noreply.github.com>
2025-06-29 12:36:05 +0200

    Edit Vehicles and Sites

--- src/main/java/it/uniroma3/siw/controller/VehicleController.java
+++ src/main/java/it/uniroma3/siw/controller/VehicleController.java
@@ -108,6 +108,7 @@ public class VehicleController {
 		existingVehicle.setColor(vehicle.getColor());
 		existingVehicle.setSeats(vehicle.getSeats());
 		existingVehicle.setPrice(vehicle.getPrice());
+		existingVehicle.setSite(vehicle.getSite());
 
 		if(!file.isEmpty()) {
 			try{
